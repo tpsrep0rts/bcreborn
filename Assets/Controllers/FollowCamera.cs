dddded1c8a14fe7d946e50454993256c6d4f36be
tpsrep0rts <brian@zanimation.com>
2019-08-27 21:21:47 -0700

    initial commit

---
+++ Assets/Controllers/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public TankController following;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(
+            following.transform.position.x,
+            transform.position.y,
+            following.transform.position.z
+            );
+    }
+}
---
+++ Assets/Controllers/TankController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public float speed;
+    public float maxSpeed;
+    public float rotationSpeed;
+    public float maxRotationSpeed;
+    public float direction;
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotationSpeed = maxRotationSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotationSpeed = -maxRotationSpeed;
+        } else
+        {
+            rotationSpeed = 0;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed = maxSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            speed = -maxSpeed;
+        }
+        else
+        {
+            speed = 0;
+        }
+    }
+    void FixedUpdate()
+    {
+        direction += rotationSpeed;
+        transform.Rotate(0, -rotationSpeed, 0, Space.Self);
+        Vector3 forward = new Vector3(
+            Mathf.Cos(direction * Mathf.PI / 180),
+            0,
+            Mathf.Sin(direction * Mathf.PI / 180)
+        );
+        characterController.SimpleMove(forward * speed * Time.deltaTime);
+    }
+}
